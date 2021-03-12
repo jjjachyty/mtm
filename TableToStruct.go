463c021d27c0264f9c05f8d98b0449994efab061
ct <739481102@qq.com>
2021-03-12 19:22:59 +0800

    修改结构

--- TableToStruct.go
+++ TableToStruct.go
@@ -137,6 +137,7 @@ func (t2s *TableToStruct) Run() error {
 			"	return" + " &pb." + structName + "{\n"
 		funcPbs := "type " + structName + "s  []*" + structName + "\n\n" +
 			"func (v " + structName + "s" + ")" + "Pbs" + "()" + "(data []*pb." + structName + "){" + "\n" + "" +
+			"   data = make([]*pb." + structName + "," + "0)" +
 			"	for _,v := range v { \n" +
 			"		data = append(data," + "&pb." + structName + "{\n"
 		for columns.Next() {
